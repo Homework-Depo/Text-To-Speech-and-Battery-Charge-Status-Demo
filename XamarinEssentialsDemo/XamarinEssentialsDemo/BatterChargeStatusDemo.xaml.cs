25b5210c8af8d62136a74c23d2938d06f7a5e185
javier-aponte <31341776+javier-aponte@users.noreply.github.com>
2022-10-13 17:14:08 -0500

    Add project files.

---
+++ XamarinEssentialsDemo/XamarinEssentialsDemo/BatterChargeStatusDemo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+[assembly: UsesPermission(Android.Manifest.Permission.BatteryStats)]
+namespace XamarinEssentialsDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BatterChargeStatusDemo : ContentPage
+    {
+        public BatterChargeStatusDemo()
+        {
+            InitializeComponent();
+
+            txtBatteryLevel.Text = (Battery.ChargeLevel * 100).ToString();
+            txtBatteryState.Text = Battery.State.ToString();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            var state = Battery.State;
+
+            txtBatteryLevel.Text = (e.ChargeLevel * 100).ToString() + "%";
+            txtBatteryState.Text = Battery.State.ToString();
+        }
+    }
+}
---
+++ XamarinEssentialsDemo/XamarinEssentialsDemo/TextToSpeechDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentialsDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextToSpeechDemo : ContentPage
+    {
+        public TextToSpeechDemo()
+        {
+            InitializeComponent();
+
+            btnSpeak.Clicked += async (sender, e) =>
+            {
+                await TextToSpeech.SpeakAsync(entryText.Text);
+            };
+        }
+    }
+}
